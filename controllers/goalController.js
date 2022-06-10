e9d910ae1a900d2b5c1182dbfa5111730508081f
ariw96 <ariw96@gmail.com>
2022-06-10 11:47:55 +0300

    initial rest api

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 // des get goal
 // route GET /api/goals
 // access private
 const getGoals = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Get goals" });
+	const goals = await Goal.find();
+	res.status(200).json(goals);
 });
 // des set goal
 // route POST /api/goals
@@ -12,19 +14,36 @@ const setGoal = asyncHandler(async (req, res) => {
 	if (!req.body.text) {
 		throw new Error("Please provide a text");
 	}
-	res.status(200).json({ message: `Set goal ` });
+	const goal = await Goal.create({
+		text: req.body.text,
+	});
+	res.status(200).json(goal);
 });
 // des update goal
 // route PUT /api/goals/:id
 // access private
 const updateGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Update goal ${req.params.id}` });
+	const goal = await Goal.findById(req.params.id);
+	if (!goal) {
+		res.status(404);
+		throw new Error("Goal not found");
+	}
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	res.status(200).json(updatedGoal);
 });
 // des get goal
 // route DELETE /api/goals/:id
 // access private
 const deleteGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Del goal ${req.params.id}` });
+	const goal = await Goal.findById(req.params.id);
+	if (!goal) {
+		res.status(404);
+		throw new Error("Goal not found");
+	}
+	await goal.remove();
+	res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const app = express();
+const colors = require("colors");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+connectDB();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
